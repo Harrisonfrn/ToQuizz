aedf0f26bce636a2a50f0a2f69076286a1e006ff
Harrison <frnharrison@gmail.com>
2018-10-23 17:32:32 +0200

    OPC Course second commit

--- app/src/main/java/com/wedevtogether/topquiz/topquiz/MainActivity.java
+++ app/src/main/java/com/wedevtogether/topquiz/topquiz/MainActivity.java
@@ -2,11 +2,44 @@ package com.wedevtogether.topquiz.topquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView mGreetingText;
+    private EditText mNameInput;
+    private Button mPlayButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mGreetingText = (TextView) findViewById(R.id.activity_main_greeting_txt);
+        mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
+        mPlayButton = (Button) findViewById(R.id.activity_main_play_btn);
+        mPlayButton.setEnabled(false);
+
+        mNameInput.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
